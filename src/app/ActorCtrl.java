d89e4af53fc0df3760b98f0fb16e3c2f718d4aca
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-03-09 20:15:26 +0100

    finished ActorCtrl, started ProductionCtrl

--- src/app/ActorCtrl.java
+++ src/app/ActorCtrl.java
@@ -1,5 +1,36 @@
 package app;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
 public class ActorCtrl extends DBConn{
-    
+    public List<String> fetchRoles(String actorName){
+        List<String> roleNames = new ArrayList<>();
+        try{
+            Statement statement = conn.createStatement();
+            String query = "select RolleIFilm.Rollenavn from (RolleIFilm natural join FilmPerson) where FilmPerson.Navn='" + actorName + "'";
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()){
+                roleNames.add(result.getString("Rollenavn"));
+            }
+        }catch(Exception e){
+            System.out.println("Database error when selecting for role names:\n" + e.getMessage());
+        }
+        return roleNames;
+    }
+    public List<String> fetchMovies(String actorName){
+        List<String> movieTitles = new ArrayList<>();
+        try{
+            Statement statement = conn.createStatement();
+            String query = "select Film.Tittel from (Film natural join (RolleIFilm natural join FilmPerson)) where (FilmPerson.Navn='" + actorName + "')";
+            ResultSet result = statement.executeQuery(query);
+            while(result.next()){
+                movieTitles.add(result.getString("Tittel"));
+            }
+        }catch(Exception e){
+            System.out.println("Database error when selecting for movie titles:\n" + e.getMessage());
+        }
+        return movieTitles;
+    }
 }
--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,7 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        ActorCtrl actorCtrl = new ActorCtrl();
+        System.out.println(actorCtrl.fetchRoles("Leonardo DiCaprio"));
     }
 }
