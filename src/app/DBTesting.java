750a5c475b1c2ea1e168640f3e82e4996fec9a52
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-03-11 22:07:36 +0100

    genre leading companies fetch

---
+++ src/app/DBTesting.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.util.List;
+import java.util.Map;
+
+public class DBTesting{
+    public static void main(String[] args){
+        ActorCtrl actorCtrl = new ActorCtrl();
+        ProductionCtrl prodctrl = new ProductionCtrl();
+        actorCtrl.connect();
+        prodctrl.connect();
+        List<String> hermannsRoller = actorCtrl.fetchRoles("Hermann");
+        for(String rolle : hermannsRoller){
+            System.out.println(rolle);
+        }
+        List<String> hermannsFilmer = actorCtrl.fetchMovies("Hermann");
+        for(String film : hermannsFilmer){
+            System.out.println(film);
+        }
+        Map<String, String> genreLeadingCompanies = prodctrl.fetchGenreLeadingCompanies();
+        for (String key : genreLeadingCompanies.keySet()){
+            System.out.println(key + ":" + genreLeadingCompanies.get(key));
+        }
+    }
+}
--- src/app/ProductionCtrl.java
+++ src/app/ProductionCtrl.java
@@ -2,21 +2,76 @@ package app;
 
 import java.sql.*;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class ProductionCtrl extends DBConn{
-    public Map<String, String> fetchGenreLeadingCompanies(){
-        Map<String, String> genreLeadingCompanies = new HashMap<>();
+    public Map<String,String> fetchGenres(){
+        Map<String,String> genres = new HashMap<>();
         try{
-            Statement statement = conn.createStatement();
-            String query = "select KategoriNavn, Selskapsnavn from (Selskap natural join (Film natural join (Filmkategori natural join Kategori))) where ";
+            Statement statement = conn.createStatement();   
+            String query = "select KategoriID, KategoriNavn from Kategori";
             ResultSet result = statement.executeQuery(query);
             while(result.next()){
-                genreLeadingCompanies.put("KategoriNavn", "Selskapsnavn");
+                genres.put(result.getString("KategoriID"),result.getString("KategoriNavn"));
             }
+        }catch(Exception e){
+            System.out.println("Database error when fetching all genres:\n" + e);
+        }
+        return genres;
+    }
+
+    public String fetchOneGenreLeadingCompany(String kategoriID){
+        String selskap = "";
+
+        try{
+            Statement statement = conn.createStatement();
+            String query = "select Selskapsnavn from (Selskap natural join (Film natural join Filmkategori)) where Filmkategori.KategoriID ='" + kategoriID + "'";
+            ResultSet result = statement.executeQuery(query);
+            List<String> companyList = new ArrayList<>();
+            while(result.next()){
+                companyList.add(result.getString("Selskapsnavn"));
+            }
+            Map<String, Integer> frequencymap = new HashMap<>();
+            for(String s : companyList) {
+                if(frequencymap.containsKey(s)) {
+                  frequencymap.put(s, frequencymap.get(s)+1);
+                }
+                else{frequencymap.put(s, 1); }
+              }
+            
+            boolean first = true;
+            selskap = "";
+            Integer currNumber = 0;
+
+            for(String key : frequencymap.keySet()){
+                if (first){
+                    selskap = key;
+                    currNumber = frequencymap.get(key);
+                    first = false;
+                }
+                if (frequencymap.get(key)>currNumber){
+                    selskap = key;
+                    currNumber = frequencymap.get(key);
+                }
+
+             }
         }catch(Exception e){
             System.out.println("Database error when selecting for genre leading companies\n" + e.getMessage());
         }
+        return selskap;
+    }
+    
+
+    public Map<String, String> fetchGenreLeadingCompanies(){
+        Map<String, String> genreLeadingCompanies = new HashMap<>();
+        Map<String,String> allGenres = fetchGenres();
+        for (String kategoriID : allGenres.keySet()){
+            String selskap = fetchOneGenreLeadingCompany(kategoriID);
+            genreLeadingCompanies.put(allGenres.get(kategoriID), selskap);
+        }
+
         return genreLeadingCompanies;
     }
 }
